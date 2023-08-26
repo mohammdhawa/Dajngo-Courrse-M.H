219629cb224e1b8b1e0335181f1ef530139c7279
Muhammed Recep Hava <ragab5434@gmail.com>
2023-08-26 14:44:53 +0300

    edited the file and added a small guessing game in it

--- firstPro.py
+++ firstPro.py
@@ -1,6 +1,20 @@
-print(50*"*")
-print("\n")
+print("\n\n")
+print("Hello, We'll play a small game with")
+print("You will guess a number from 1 to 100")
+print("if you guessed the true number you'll win")
+print("be carefull that you have just 5 attempts.")
 
-print("\t\tHello World!!!\n\n")
+from random import randint
+number = randint(1, 100)
+print(number)
 
-print(50*"*")
+i = 0
+while i < 5:
+    x = int(input(f"{i+1}.attempt, guess a number: "))
+    if x == number:
+        print("congratulations, You win")
+        break
+    i += 1
+    
+if i == 5:
+    print("Game over!")
